2c4edd5f5d3c3f75e2aa3e2c47869fa0581d5104
Joseph Abell <joseph.abell@piksel.com>
2017-10-16 20:58:34 +0100

    remove lazy loading as jest disliked it

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,35 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import firebase from 'firebase'
 
+import Landing from '@/components/Landing'
+import Signin from '@/components/Signin'
+import Signup from '@/components/Signup'
+import Home from '@/components/Home'
+import Data from '@/components/Data'
+import DataSingleEdit from '@/components/DataSingleEdit'
+import DataMultiple from '@/components/DataMultiple'
+import DataMultipleEdit from '@/components/DataMultipleEdit'
+import Images from '@/components/Images'
+import ContentTypes from '@/components/ContentTypes'
+import ContentTypeEdit from '@/components/ContentTypeEdit'
 import NotFound from '@/components/NotFound'
-
-const routerOptions = [
-  { path: '/', component: 'Landing', meta: { requiresUnauth: true } },
-  { path: '/signin', component: 'Signin' },
-  { path: '/signup', component: 'Signup' },
-  { path: '/home', component: 'Home', meta: { requiresAuth: true } },
-  { path: '/data', component: 'Data', meta: { requiresAuth: true } },
-  { path: '/data/edit/:dataId', component: 'DataSingleEdit', meta: { requiresAuth: true } },
-  { path: '/data/:dataId', component: 'DataMultiple', meta: { requiresAuth: true } },
-  { path: '/data/:dataId/:dataItemId', component: 'DataMultipleEdit', meta: { requiresAuth: true } },
-  { path: '/images', component: 'Images', meta: { requiresAuth: true } },
-  { path: '/content-types', component: 'ContentTypes', meta: { requiresAuth: true } },
-  { path: '/content-types/:contentTypeId', component: 'ContentTypeEdit', meta: { requiresAuth: true } }
-]
-
-const routes = routerOptions.map(route => ({
-  path: route.path,
-  component: () => import(`@/components/${route.component}`),
-  meta: route.meta
-}))
 
 Vue.use(Router)
 
 const router = new Router({
   mode: 'history',
   routes: [
-    ...routes,
+    { path: '/', component: Landing, meta: { requiresUnauth: true } },
+    { path: '/signin', component: Signin },
+    { path: '/signup', component: Signup },
+    { path: '/home', component: Home, meta: { requiresAuth: true } },
+    { path: '/data', component: Data, meta: { requiresAuth: true } },
+    { path: '/data/edit/:dataId', component: DataSingleEdit, meta: { requiresAuth: true } },
+    { path: '/data/:dataId', component: DataMultiple, meta: { requiresAuth: true } },
+    { path: '/data/:dataId/:dataItemId', component: DataMultipleEdit, meta: { requiresAuth: true } },
+    { path: '/images', component: Images, meta: { requiresAuth: true } },
+    { path: '/content-types', component: ContentTypes, meta: { requiresAuth: true } },
+    { path: '/content-types/:contentTypeId', component: ContentTypeEdit, meta: { requiresAuth: true } },
     { path: '*', component: NotFound }
   ]
 })
